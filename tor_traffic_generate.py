3aeb1a666b437df49a63033890ddf7cf33eb466f
arjun Agarwal <arjunagarwal1998@gmail.com>
2022-07-19 22:06:59 +0100

    add additional fields

--- tor_traffic_generate.py
+++ tor_traffic_generate.py
@@ -3,11 +3,13 @@ import time
 import subprocess
 import traceback
 import uuid
+
+torBundlePath = "/home/arjun/Downloads/tor-browser_en-US/Browser/start-tor-browser"
 def startSession():
     try:
         uid = uuid.uuid4()
         proc = subprocess.Popen(['tcpdump','-i','enp0s9', '-w','dataset/'+str(uid)+'.pcap'])
-        browserProcess = subprocess.Popen(['/home/arjun/Downloads/tor-browser_en-US/Browser/start-tor-browser','--detach'])
+        browserProcess = subprocess.Popen([torBundlePath,'--detach'])
         time.sleep(10)
         subprocess.Popen(['pkill','-15', 'firefox'])
         browserProcess.terminate()
