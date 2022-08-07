97e908f5e358b2bb09b5da1045013dc24eccfdc2
arjun Agarwal <arjunagarwal1998@gmail.com>
2022-08-07 15:37:50 +0100

    feat: generate chart after test run

--- time_experiment.py
+++ time_experiment.py
@@ -2,6 +2,8 @@ import pandas as pd
 from classify import load_models, preprocess
 import time
 import numpy as np
+import matplotlib.pyplot as plt
+
 def run_experiment():
     df = pd.read_csv('csv/training.csv')
     df_sample = df.sample(n = 300, random_state= 100)
@@ -15,7 +17,13 @@ def run_experiment():
             models[model_name].predict(X)
             time_array.append((time.time() - start_time) * 1000)
         score_time_dict[model_name] = np.mean(time_array)
-    print(score_time_dict)
+    show_charts(score_time_dict)
+
+def show_charts(score_dict):
+    y = score_dict.values()
+    x = score_dict.keys()
+    plt.bar(x, y)
+    plt.show()
 
 if __name__ == '__main__':
     run_experiment()
